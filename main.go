f89a4b237014558efcbfe89bd9df949c424f2c01
Hadidomena <szepiet33@gmail.com>
2026-06-03 16:03:58 +0200

    for now leave only tea UI in main.go

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"os"
 
 	tea "charm.land/bubbletea/v2"
-	"github.com/Hadidomena/tuiFeed/bsk"
 )
 
 type model struct {
@@ -70,41 +68,6 @@ func (m model) View() tea.View {
 }
 
 func main() {
-	postUrl := "https://bsky.app/profile/p-e-g-76.bsky.social/post/3mcncopydnc2g"
-	ctx := context.Background()
-	client := bsk.NewClient()
-
-	thread, err := bsk.GetPostThread(ctx, client, postUrl)
-	if err != nil {
-		panic(fmt.Errorf("error fetching post: %w", err))
-	}
-
-	if thread.Parent != nil {
-		fmt.Println("--- PARENT POST (REPLY TO) ---")
-		fmt.Printf("Author: %s (@%s)\n", thread.Parent.AuthorDisplayName, thread.Parent.AuthorHandle)
-		fmt.Printf("Content: %s\n", thread.Parent.Text)
-		fmt.Printf("❤️  %d | 💬 %d\n", thread.Parent.LikeCount, thread.Parent.ReplyCount)
-		fmt.Printf("Embeds: %+v\n", thread.Parent.Embeds)
-	}
-
-	fmt.Println("--- FOUND POST ---")
-	fmt.Printf("Author: %s (@%s)\n", thread.Post.AuthorDisplayName, thread.Post.AuthorHandle)
-	fmt.Printf("Content: %s\n", thread.Post.Text)
-	fmt.Printf("Likes: %d\n", thread.Post.LikeCount)
-	fmt.Printf("Date:  %s\n", thread.Post.CreatedAt)
-	fmt.Printf("Embeds: %+v\n", thread.Post.Embeds)
-
-	if len(thread.Replies) > 0 {
-		fmt.Printf("\n--- REPLIES (%d) ---\n", len(thread.Replies))
-		for i, reply := range thread.Replies {
-			fmt.Printf("\n[%d] %s (@%s):\n", i+1, reply.AuthorDisplayName, reply.AuthorHandle)
-			fmt.Printf("    %s\n", reply.Text)
-			fmt.Printf("    ❤️ %d | 💬 %d\n", reply.LikeCount, reply.ReplyCount)
-		}
-	} else {
-		fmt.Println("\nNo replies.")
-	}
-
 	p := tea.NewProgram(initialModel())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
