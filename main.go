30174b48a16bea749bd0ebdcfb2e1c8498335a41
Hadidomena <szepiet33@gmail.com>
2026-08-18 10:59:59 +0200

    incorporate rss into the main structure

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ import (
 	"github.com/Hadidomena/tuiFeed/config"
 	"github.com/Hadidomena/tuiFeed/feed"
 	"github.com/Hadidomena/tuiFeed/follows"
+	"github.com/Hadidomena/tuiFeed/rss"
+	"github.com/Hadidomena/tuiFeed/rssfeed"
+	"github.com/Hadidomena/tuiFeed/rssfeeds"
 	"github.com/Hadidomena/tuiFeed/thread"
 	"github.com/Hadidomena/tuiFeed/utils"
 )
@@ -26,22 +29,41 @@ const (
 	showLoadingView
 	showSavedPostsView
 	showThreadView
+	showRSSView
+	showRSSFeedSelectView
+	showRSSSinceLastCheckView
+	showRSSManageView
+	showSavedRSSView
 )
 
 type OpenFollowsMsg struct{}
 type OpenFeedMsg struct{}
 type OpenAccountSelectMsg struct{}
 type OpenSavedPostsMsg struct{}
+type OpenRSSMsg struct{}
+type OpenRSSManageMsg struct{}
+type OpenRSSSelectMsg struct{}
+type OpenSavedRSSMsg struct{}
 type BackToDashboardMsg struct{}
 
 type SelectAccountForSinceLastCheck struct {
 	handle string
 }
 
+type SelectRSSFeedForSinceLastCheck struct {
+	url string
+}
+
 type PostsFetchedMsg struct {
 	handle string
 	posts  []bsk.FeedItem
 	err    error
+}
+
+type RSSEntriesFetchedMsg struct {
+	url     string
+	entries []rss.Entry
+	err     error
 }
 
 type DashboardModel struct {
@@ -53,7 +75,7 @@ type DashboardModel struct {
 
 func NewDashboardModel() DashboardModel {
 	return DashboardModel{
-		choices: []string{"View Feed", "Posts since last check", "Manage follows", "Saved posts"},
+		choices: []string{"View Feed", "Posts since last check", "Manage follows", "Saved posts", "View RSS feeds", "RSS since last check", "Manage RSS feeds", "Saved RSS entries"},
 		width:   utils.DefaultWidth,
 		height:  24,
 	}
@@ -88,6 +110,18 @@ func (m DashboardModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			}
 			if m.cursor == 3 {
 				return m, func() tea.Msg { return OpenSavedPostsMsg{} }
+			}
+			if m.cursor == 4 {
+				return m, func() tea.Msg { return OpenRSSMsg{} }
+			}
+			if m.cursor == 5 {
+				return m, func() tea.Msg { return OpenRSSSelectMsg{} }
+			}
+			if m.cursor == 6 {
+				return m, func() tea.Msg { return OpenRSSManageMsg{} }
+			}
+			if m.cursor == 7 {
+				return m, func() tea.Msg { return OpenSavedRSSMsg{} }
 			}
 		}
 	}
@@ -182,6 +216,80 @@ func (m AccountSelectModel) View() tea.View {
 	return tea.NewView(utils.CenterBlock(b.String(), m.width))
 }
 
+type RSSFeedSelectModel struct {
+	feeds      []string
+	lastChecks map[string]string
+	cursor     int
+	width      int
+	height     int
+}
+
+func NewRSSFeedSelectModel(cfg *config.Config) RSSFeedSelectModel {
+	return RSSFeedSelectModel{
+		feeds:      cfg.RSSFeeds,
+		lastChecks: cfg.RSSLastChecks,
+		width:      utils.DefaultWidth,
+		height:     24,
+	}
+}
+
+func (m RSSFeedSelectModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m RSSFeedSelectModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+		return m, nil
+	case tea.KeyPressMsg:
+		switch msg.String() {
+		case "esc":
+			return m, func() tea.Msg { return BackToDashboardMsg{} }
+		case "up", "k":
+			m.cursor = utils.CursorUp(m.cursor)
+		case "down", "j":
+			m.cursor = utils.CursorDown(m.cursor, len(m.feeds))
+		case "enter":
+			if len(m.feeds) > 0 {
+				return m, func() tea.Msg {
+					return SelectRSSFeedForSinceLastCheck{url: m.feeds[m.cursor]}
+				}
+			}
+		}
+	}
+	return m, nil
+}
+
+func (m RSSFeedSelectModel) View() tea.View {
+	var b strings.Builder
+
+	utils.WriteHeader(&b, "RSS since last check", m.width)
+
+	if len(m.feeds) == 0 {
+		b.WriteString("  No RSS feeds subscribed.\n")
+		b.WriteString("  Add feeds in 'Manage RSS feeds' first.\n\n")
+	} else {
+		for i, url := range m.feeds {
+			cursor := "  "
+			if m.cursor == i {
+				cursor = "> "
+			}
+			lastStr := "never"
+			if t, ok := m.lastChecks[url]; ok && t != "" {
+				if len(t) >= 10 {
+					lastStr = t[:10]
+				}
+			}
+			fmt.Fprintf(&b, "%s%s  (last: %s)\n", cursor, url, lastStr)
+		}
+	}
+
+	b.WriteString("\n[\u2191/\u2193] navigate  [enter] select  [esc] back\n")
+	return tea.NewView(utils.CenterBlock(b.String(), m.width))
+}
+
 type LoadingModel struct {
 	message string
 	width   int
@@ -214,8 +322,11 @@ type MainModel struct {
 	dashboard     DashboardModel
 	follows       follows.Model
 	accountSelect AccountSelectModel
+	rssFeedSelect RSSFeedSelectModel
 	feed          feed.Model
 	thread        thread.Model
+	rssfeed       rssfeed.Model
+	rssfeeds      rssfeeds.Model
 	loading       LoadingModel
 	width         int
 	height        int
@@ -331,6 +442,55 @@ func (m MainModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			m.feed = feed.NewStaticModel(msg.posts, fmt.Sprintf("Posts since last check \u2014 @%s", msg.handle)).WithConfig(m.cfg).WithSize(m.width, m.height)
 		}
 		return m, m.feed.Init()
+	case OpenRSSMsg:
+		m.state = showRSSView
+		rf, err := rssfeed.NewModel()
+		if err == nil {
+			m.rssfeed = rf.WithSize(m.width, m.height)
+		}
+		return m, m.rssfeed.Init()
+	case OpenRSSManageMsg:
+		m.state = showRSSManageView
+		rf, err := rssfeeds.NewModel()
+		if err == nil {
+			m.rssfeeds = rf.WithSize(m.width, m.height)
+		}
+		return m, m.rssfeeds.Init()
+	case OpenRSSSelectMsg:
+		m.state = showRSSFeedSelectView
+		m.cfg, _ = config.Load()
+		m.rssFeedSelect = NewRSSFeedSelectModel(m.cfg)
+		m.rssFeedSelect.width = m.width
+		m.rssFeedSelect.height = m.height
+		return m, m.rssFeedSelect.Init()
+	case OpenSavedRSSMsg:
+		m.state = showSavedRSSView
+		m.cfg, _ = config.Load()
+		m.rssfeed = rssfeed.NewSavedModel(m.cfg).WithSize(m.width, m.height)
+		return m, m.rssfeed.Init()
+	case rssfeed.BackMsg, rssfeeds.BackMsg:
+		m.state = showDashboardView
+		m.dashboard.width = m.width
+		m.dashboard.height = m.height
+		return m, nil
+	case SelectRSSFeedForSinceLastCheck:
+		m.state = showLoadingView
+		m.loading = LoadingModel{message: fmt.Sprintf("Fetching entries from %s...", msg.url), width: m.width, height: m.height}
+		m.cfg, _ = config.Load()
+		lastCheck := ""
+		if m.cfg.RSSLastChecks != nil {
+			lastCheck = m.cfg.RSSLastChecks[msg.url]
+		}
+		return m, fetchRSSSinceLastCheckCmd(msg.url, lastCheck)
+	case RSSEntriesFetchedMsg:
+		m.state = showRSSSinceLastCheckView
+		m.cfg, _ = config.Load()
+		if msg.err != nil {
+			m.rssfeed = rssfeed.NewStaticModel(nil, fmt.Sprintf("Error: %v", msg.err)).WithSize(m.width, m.height)
+		} else {
+			m.rssfeed = rssfeed.NewStaticModel(msg.entries, fmt.Sprintf("RSS since last check \u2014 %s", msg.url)).WithConfig(m.cfg).WithSize(m.width, m.height)
+		}
+		return m, m.rssfeed.Init()
 	}
 
 	switch m.state {
@@ -342,12 +502,22 @@ func (m MainModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.feed, cmd = updateSubModel(m.feed, msg)
 	case showAccountSelectView:
 		m.accountSelect, cmd = updateSubModel(m.accountSelect, msg)
+	case showRSSFeedSelectView:
+		m.rssFeedSelect, cmd = updateSubModel(m.rssFeedSelect, msg)
 	case showSinceLastCheckView:
 		m.feed, cmd = updateSubModel(m.feed, msg)
 	case showSavedPostsView:
 		m.feed, cmd = updateSubModel(m.feed, msg)
 	case showThreadView:
 		m.thread, cmd = updateSubModel(m.thread, msg)
+	case showRSSView:
+		m.rssfeed, cmd = updateSubModel(m.rssfeed, msg)
+	case showRSSSinceLastCheckView:
+		m.rssfeed, cmd = updateSubModel(m.rssfeed, msg)
+	case showSavedRSSView:
+		m.rssfeed, cmd = updateSubModel(m.rssfeed, msg)
+	case showRSSManageView:
+		m.rssfeeds, cmd = updateSubModel(m.rssfeeds, msg)
 	}
 
 	return m, cmd
@@ -368,12 +538,22 @@ func (m MainModel) View() tea.View {
 		return m.feed.View()
 	case showAccountSelectView:
 		return m.accountSelect.View()
+	case showRSSFeedSelectView:
+		return m.rssFeedSelect.View()
 	case showSinceLastCheckView:
 		return m.feed.View()
 	case showSavedPostsView:
 		return m.feed.View()
 	case showThreadView:
 		return m.thread.View()
+	case showRSSView:
+		return m.rssfeed.View()
+	case showRSSSinceLastCheckView:
+		return m.rssfeed.View()
+	case showSavedRSSView:
+		return m.rssfeed.View()
+	case showRSSManageView:
+		return m.rssfeeds.View()
 	case showLoadingView:
 		return m.loading.View()
 	default:
@@ -403,6 +583,30 @@ func fetchSinceLastCheckCmd(handle string, lastCheck string) tea.Cmd {
 			}
 		}
 		return PostsFetchedMsg{handle: handle, posts: filtered}
+	}
+}
+
+func fetchRSSSinceLastCheckCmd(url string, lastCheck string) tea.Cmd {
+	return func() tea.Msg {
+		entries, err := rss.FetchFeed(context.Background(), url)
+		if err != nil {
+			return RSSEntriesFetchedMsg{url: url, err: err}
+		}
+		if err := config.Update(func(cfg *config.Config) {
+			cfg.SetRSSLastCheck(url)
+		}); err != nil {
+			fmt.Fprintf(os.Stderr, "warning: failed to update last-check for %s: %v\n", url, err)
+		}
+		if lastCheck == "" {
+			return RSSEntriesFetchedMsg{url: url, entries: entries}
+		}
+		filtered := make([]rss.Entry, 0, len(entries))
+		for _, e := range entries {
+			if e.Published > lastCheck {
+				filtered = append(filtered, e)
+			}
+		}
+		return RSSEntriesFetchedMsg{url: url, entries: filtered}
 	}
 }
 
